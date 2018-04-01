5643a801dd72dda80b24202bf15fa6dc3ea0cbf6
Edi Rose <edirose1998@gmail.com>
2018-04-02 11:21:11 +1200

    first (failed) attempts

--- app.js
+++ app.js
@@ -1,9 +1,28 @@
-const brain = requre('brain.js')
+const brain = require('brain.js')
 
-const network = new brain.neuralNetwork()
+const network = new brain.NeuralNetwork()
 
 network.train([
-  {input:[] output:[]}
+  {input:[['O', 'O', ''],
+          ['', '', ''],
+          ['', '', '']], output:[['O', 'O', 'X'],
+                                 ['', '', ''],
+                                 ['', '', '']]},
+  {input:[['', '', ''],
+          ['', 'O', ''],
+          ['O', '', '']], output:[['', '', 'X'],
+                                 ['', 'O', ''],
+                                 ['O', '', '']]},
+{input:[['', '', ''],
+        ['', 'O', ''],
+        ['O', '', '']], output:[['', '', 'X'],
+                               ['', 'O', ''],
+                               ['O', '', '']]},
 ])
 
-const result = network.run()
+const result = network.run([['', '', ''],
+                            ['O', 'O', ''],
+                            ['', '', '']])
+
+
+console.log(result)
