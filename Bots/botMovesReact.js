24f6f4d2021cfc49477a2a534e83ea35ca5a4643
Edi Rose <edirose1998@gmail.com>
2018-04-06 13:42:46 +1200

    added mockBoards, outputs now returning objects

--- Bots/botMovesReact.js
+++ Bots/botMovesReact.js
@@ -6,28 +6,24 @@ var board = require('../board')
 
 function crossMove(board){
       if(attack('cross', board)){
-        board[attack('cross', board)].teamName ='cross'
         if(attack('cross', board) == false){
           console.log('attack')
         }
         return attack('cross', board)
       }
       else if(defend('cross', board)){
-        board[defend('cross', board)].teamName ='cross'
         if(defend('cross', board) == false){
           console.log('defense')
         }
         return defend('cross', board)
         }
         else if(smartMove('cross', board)){
-          board[smartMove('cross', board)].teamName ='cross'
           if(smartMove('cross', board) == false){
             console.log('smartMove cross')
           }
           return smartMove('cross', board)
         }
         else if(smartMove('naught', board)){
-          board[smartMove('naught', board)].teamName ='cross'
           if(smartMove('naught', board) == false){
             console.log('smartMove naught')
           }
@@ -36,7 +32,6 @@ function crossMove(board){
       else {
         for (var i = 0; i < board.length; i++) {
           if(board[i].teamName == 'none'){
-            board[i].teamName = 'cross'
             return i
           }
         }
--- app.js
+++ app.js
@@ -1,60 +1,29 @@
 const brain = require('brain.js')
 const crossMove = require('./Bots/botMovesReact')
-const makeBoards = require('./randomizer')
+const mockBoards = require('./mockBoards')
 const board = require('./board')
-
-let boards = makeBoards(9, board)
+const squares = require('./squares')
 
 const network = new brain.NeuralNetwork()
 
 network.train([
- {input:[boards[0]], output: [crossMove(boards[0])]},
- {input:[boards[1]], output: [crossMove(boards[1])]},
- {input:[boards[2]], output: [crossMove(boards[2])]},
- {input:[boards[3]], output: [crossMove(boards[3])]},
- {input:[boards[4]], output: [crossMove(boards[4])]},
- {input:[boards[5]], output: [crossMove(boards[5])]},
- {input:[boards[6]], output: [crossMove(boards[6])]},
- {input:[boards[7]], output: [crossMove(boards[7])]}
+ {input:[mockBoards[0]], output: [getSquare(crossMove(mockBoards[0]))]},
+ {input:[mockBoards[1]], output: [getSquare(crossMove(mockBoards[1]))]},
+ {input:[mockBoards[2]], output: [getSquare(crossMove(mockBoards[2]))]},
+ {input:[mockBoards[3]], output: [getSquare(crossMove(mockBoards[3]))]},
+ {input:[mockBoards[4]], output: [getSquare(crossMove(mockBoards[4]))]},
+ {input:[mockBoards[5]], output: [getSquare(crossMove(mockBoards[5]))]},
+ {input:[mockBoards[6]], output: [getSquare(crossMove(mockBoards[6]))]},
+ {input:[mockBoards[7]], output: [getSquare(crossMove(mockBoards[7]))]}
 ])
 
-const result = network.run([boards[8]])
 
-class Square {
-  constructor(id, name){
-    this.prob = 0
-    this.id = id
-    this.name = name
-  }
-}
+const result = network.run([mockBoards[8]])
 
-let tl = new Square(0, 'tl')
-let tm = new Square(1, 'tm')
-let tr = new Square(2, 'tr')
-let ml = new Square(3, 'ml')
-let mm = new Square(4, 'mm')
-let mr = new Square(5, 'mr')
-let bl = new Square(6, 'bl')
-let bm = new Square(7, 'bm')
-let br = new Square(8, 'br')
-
-let squares = [tl, tm, tr, ml, mm, mr, bl, bm, br]
-
+console.log(result)
 function getSquare(num){
   let theGuy = squares.filter(square => square.id == num)
-  return theGuy[0].name + ':'+ '1'
+  let obj ={}
+  obj[theGuy[0].name] = 1
+  return obj
 }
-//console.log(squares[1].name)
-console.log(getSquare(2))
-// console.log('square: ', squares)
-
-
-function check(){
-  for (var i = 0; i < 7; i++) {
-    if(crossMove(boards[i]) == false){
-      console.log('false!: ', boards[i])
-    }
-  }
-}
-
-// check()
