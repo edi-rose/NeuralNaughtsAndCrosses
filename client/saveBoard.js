119845b933e4b588ebd2790d38525c1818730650
Edi Rose <edirose1998@gmail.com>
2018-05-01 23:24:35 +1200

    added arrangeBoard and boardToInput functions

--- client/saveBoard.js
+++ client/saveBoard.js
@@ -1,5 +1,15 @@
+var boardToInput = require('../network/helpers/boardToInput')
+
 function saveBoard(board){
-  console.log(board)
+  let newBoard = arrangeBoard(board)
+  let boardArray = boardToInput(newBoard)
+  console.log(boardArray)
+
+}
+
+function arrangeBoard(board){
+    let newBoard = board[0].concat(board[1], board[2])
+    return newBoard
 }
 
 module.exports = saveBoard
