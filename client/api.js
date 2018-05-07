bebf5869abcd381eb7a6504617850390dcb16d8e
Edi Rose <edirose1998@gmail.com>
2018-05-07 23:30:56 +1200

    removed console.logs

--- client/api.js
+++ client/api.js
@@ -2,7 +2,6 @@ import request from 'superagent'
 var arrangeBoard = require('./arrangeBoard')
 
 function saveBoard(board){
-  console.log(board)
   let newBoard = arrangeBoard(board)
   request
   .post('/api')
--- server/db.js
+++ server/db.js
@@ -3,8 +3,6 @@ const config = require('../knexfile')[environment]
 const connection = require('knex')(config)
 
 function addBoard(board){
-  console.log('addBoard called')
-  console.log(board.newBoard)
   const db = connection
   return connection('boards')
     .insert({board: board.newBoard})
