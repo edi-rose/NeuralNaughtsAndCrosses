5266029961e23a21b7784e77a8a1d8f6704aab3e
Edi Rose <edirose1998@gmail.com>
2018-05-11 22:44:59 +1200

    removed get idx useless function

--- client/network/resultHandlers/getNames.js
+++ client/network/resultHandlers/getNames.js
@@ -6,19 +6,6 @@ function getNames(output){
    return matchArrToNames(sortOutput(output), output)
 }
 
-function getIdx(output){
-  let resultArray = Object.values(output)
-  let currentMax = 0
-  let idxMax = 0
-  resultArray.map((x, idx) => {
-    if(x > currentMax){
-      currentMax = resultArray[idx]
-      idxMax = idx
-    }
-  })
-  return idxMax
-}
-
 function sortOutput(output){
   let sortedArray = Object.values(output)
   sortedArray.sort(function(a, b){return b - a})
